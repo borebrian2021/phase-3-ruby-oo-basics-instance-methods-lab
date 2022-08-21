48a06d240a81932ee5785ee604be88c24dcd60f9
borebrian2021 <bkimutai2021@gmail.com>
2022-08-21 07:19:18 +0300

    commit tree 7a96a66e53b323c9083c1a79d5ffb3e59c55039c

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+talk=Person.new
+talk.talk
+walk= Person.new
+walk.walk
